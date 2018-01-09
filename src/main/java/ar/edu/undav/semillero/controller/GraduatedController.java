e5121e73820175dfa7806db4c47c2462a8ec75fd
juan <jplagostena@gmail.com>
2018-01-08 23:31:34 -0300

    Agregado @CrossOrigin a los controladores

--- src/main/java/ar/edu/undav/semillero/controller/GraduatedController.java
+++ src/main/java/ar/edu/undav/semillero/controller/GraduatedController.java
@@ -9,12 +9,7 @@ import java.util.Date;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.fasterxml.jackson.annotation.JsonView;
 
@@ -26,6 +21,7 @@ import ar.edu.undav.semillero.view.View;
 
 @RestController
 @RequestMapping("/graduated")
+@CrossOrigin
 public class GraduatedController {
 
 	@Autowired
--- src/main/java/ar/edu/undav/semillero/controller/NodeController.java
+++ src/main/java/ar/edu/undav/semillero/controller/NodeController.java
@@ -7,12 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.fasterxml.jackson.annotation.JsonView;
 
@@ -22,6 +17,7 @@ import ar.edu.undav.semillero.view.View;
 
 @RestController
 @RequestMapping("/node")
+@CrossOrigin
 public class NodeController {
 
 	@Autowired
