0bb45a8a4751babb3a56cb132e8479b925829525
Alejandro Gomez <ideaplugins@gmail.com>
2018-03-30 23:40:38 -0300

    Simplificar la resolucion de la url de la BD para todos los entornos

---
+++ src/main/java/ar/edu/undav/semillero/config/HerokuAwareDataSourceProperties.java
@@ -0,0 +1,61 @@
+package ar.edu.undav.semillero.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+/**
+ * @author Alejandro Gomez
+ */
+@Primary
+@Component
+@ConfigurationProperties(prefix = "spring.datasource")
+public class HerokuAwareDataSourceProperties extends DataSourceProperties {
+
+    private final URI herokuDsUri;
+
+    public HerokuAwareDataSourceProperties(@Value("${JAWSDB_MARIA_URL:}") String herokuDsUrl) {
+        herokuDsUri = parseHerokuUrl(herokuDsUrl);
+    }
+
+    @Override
+    public String determineUrl() {
+        return determineProperty(uri -> "jdbc:mysql://" + uri.getHost() + ":" + String.valueOf(uri.getPort()) + uri.getPath(), super::determineUrl);
+    }
+
+    @Override
+    public String determineUsername() {
+        return determineProperty(uri -> uri.getUserInfo().split(":")[0], super::determineUsername);
+    }
+
+    @Override
+    public String determinePassword() {
+        return determineProperty(uri -> uri.getUserInfo().split(":")[1], super::determinePassword);
+    }
+
+    private String determineProperty(Function<URI, String> mapper, Supplier<String> supplier) {
+        return Optional.ofNullable(herokuDsUri)
+                .map(mapper)
+                .orElseGet(supplier);
+    }
+
+    private static URI parseHerokuUrl(String herokuDsUrl) {
+        if (StringUtils.hasText(herokuDsUrl)) {
+            try {
+                return new URI(herokuDsUrl);
+            } catch (URISyntaxException ex) {
+                throw new RuntimeException("Invalid jaws db url", ex);
+            }
+        }
+        return null;
+    }
+}
