bbce4fd1b43690b7777464d21eb4270d5231d502
Alejandro Gomez <ideaplugins@gmail.com>
2018-04-19 20:02:57 -0300

    Fix table name

--- src/main/resources/sql-migrations/201804151820_add_interview_fields_for_working_state.sql
+++ src/main/resources/sql-migrations/201804151820_add_interview_fields_for_working_state.sql
@@ -1,2 +1,2 @@
-ALTER TABLE graduated ADD work_from_date date DEFAULT NULL;
-ALTER TABLE graduated ADD work_to_date date DEFAULT NULL;
+ALTER TABLE interview ADD work_from_date date DEFAULT NULL;
+ALTER TABLE interview ADD work_to_date date DEFAULT NULL;
