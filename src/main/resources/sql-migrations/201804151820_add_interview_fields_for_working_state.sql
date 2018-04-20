0114de38062f71ee885d028a0d150071d08d4003
Virginia Barros <virginianbarros@gmail.com>
2018-04-20 17:11:10 -0300

    Merge remote-tracking branch 'remotes/origin/development' into model/changes
    # Conflicts:
    #	src/main/java/ar/edu/undav/semillero/domain/entity/Company.java
    #	src/main/resources/api.raml
    #	src/main/resources/sql-migrations/201804151820_add_interview_fields_for_working_state.sql

--- src/main/resources/sql-migrations/201804151820_add_interview_fields_for_working_state.sql
+++ src/main/resources/sql-migrations/201804151820_add_interview_fields_for_working_state.sql
@@ -1,2 +1,2 @@
-ALTER TABLE student ADD work_from_date date DEFAULT NULL;
-ALTER TABLE student ADD work_to_date date DEFAULT NULL;
+ALTER TABLE interview ADD work_from_date date DEFAULT NULL;
+ALTER TABLE interview ADD work_to_date date DEFAULT NULL;
