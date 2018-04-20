31da12595b6d9024b6ae4a63f2693675a2701cd9
Alejandro Gomez <ideaplugins@gmail.com>
2018-04-20 14:55:41 -0300

    Filter company list

--- src/main/java/ar/edu/undav/semillero/controller/CompanyController.java
+++ src/main/java/ar/edu/undav/semillero/controller/CompanyController.java
@@ -1,10 +1,12 @@
 package ar.edu.undav.semillero.controller;
 
 import ar.edu.undav.semillero.domain.entity.Company;
+import ar.edu.undav.semillero.dto.CompanyDTO;
 import ar.edu.undav.semillero.request.CreateCompanyRequest;
+import ar.edu.undav.semillero.request.FilterCompaniesRequest;
 import ar.edu.undav.semillero.service.CompanyService;
-import ar.edu.undav.semillero.view.View;
-import com.fasterxml.jackson.annotation.JsonView;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,13 +14,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-
-import java.util.Collection;
 
 @RestController
 @RequestMapping("/company")
@@ -44,12 +43,9 @@ public class CompanyController {
         return WebUtils.emptyToNotFound(companyService.findById(id));
     }
 
-    // Obtener todas las companias, si se pasa parametro, se devuelve por nombre
     @ResponseBody
-    @JsonView(View.Summary.class)
     @GetMapping
-    public ResponseEntity<Collection<Company>> getCompany(@RequestParam(value = "name", required = false) String name) {
-        Collection<Company> companies = name == null ? companyService.findAll() : companyService.findByName(name);
-        return WebUtils.emptyToNotFound(companies);
+    public Page<CompanyDTO> list(@Valid FilterCompaniesRequest request, Pageable pageable) {
+        return companyService.list(request, pageable);
     }
 }
