e90e573c273e4a99f74d6e8444a7de062aa87976
Luciana Dubiau <ldubiau@medallia.com>
2018-04-21 17:54:33 -0300

    students api: filters + paging + fields added

--- src/main/java/ar/edu/undav/semillero/controller/StudentController.java
+++ src/main/java/ar/edu/undav/semillero/controller/StudentController.java
@@ -2,10 +2,12 @@ package ar.edu.undav.semillero.controller;
 
 import ar.edu.undav.semillero.domain.entity.Student;
 import ar.edu.undav.semillero.request.CreateStudentRequest;
+import ar.edu.undav.semillero.request.FilterStudentsRequest;
 import ar.edu.undav.semillero.service.StudentService;
 import ar.edu.undav.semillero.view.View;
 import com.fasterxml.jackson.annotation.JsonView;
-import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -14,12 +16,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-
-import java.time.LocalDate;
 import java.util.Collection;
 
 @RestController
@@ -54,13 +53,7 @@ public class StudentController {
     // Obtener todos los graduados
     @JsonView(View.Summary.class)
     @GetMapping
-    public Collection<Student> getStudent(@DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @RequestParam(value = "when", required = false) LocalDate when, @RequestParam(value = "node", required = false) Long nodeId) {
-        if (nodeId != null) {
-            return studentService.findByNode(nodeId);
-        } else if (when != null) {
-            return studentService.findByDate(when);
-        } else {
-            return studentService.findAll();
-        }
+    public Page<Student> list(@Valid FilterStudentsRequest request, Pageable pageable) {
+       return studentService.list(request, pageable);
     }
 }
