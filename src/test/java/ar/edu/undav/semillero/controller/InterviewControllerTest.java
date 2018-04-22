fda53e131430cb75b544d7fd4d5c82970cb9f1f6
Alejandro Gomez <ideaplugins@gmail.com>
2018-04-22 11:18:16 -0300

    Add pageable workaround in tests

--- src/test/java/ar/edu/undav/semillero/controller/InterviewControllerTest.java
+++ src/test/java/ar/edu/undav/semillero/controller/InterviewControllerTest.java
@@ -8,8 +8,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -20,6 +22,7 @@ import java.time.LocalDate;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(value = InterviewController.class)
+@Import(SpringDataWebAutoConfiguration.class)
 public class InterviewControllerTest {
 
     @Autowired
