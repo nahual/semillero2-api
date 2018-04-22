e568ec6a77fd6aad3d3487edaad3d6cc955ddc02
Virginia Barros <virginianbarros@gmail.com>
2018-04-22 16:07:19 -0300

    Agregando el update de student

--- src/main/java/ar/edu/undav/semillero/controller/StudentController.java
+++ src/main/java/ar/edu/undav/semillero/controller/StudentController.java
@@ -8,15 +8,7 @@ import ar.edu.undav.semillero.service.StudentService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
@@ -54,6 +46,12 @@ public class StudentController {
     @GetMapping
     public Page<StudentDTO> list(@Valid FilterStudentsRequest request, Pageable pageable) {
        return studentService.list(request, pageable);
+    }
+
+    @ResponseBody
+    @PutMapping("/{id}")
+    public ResponseEntity<Student> update(@PathVariable long id, @Valid @RequestBody CreateStudentRequest request) {
+        return WebUtils.emptyToNotFound(studentService.update(id, request));
     }
 
     @PostMapping("/bulk")
