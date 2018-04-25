a6ae801da41ca828e8ab2f18bb7d7b62daa7a9ba
Alejandro Gomez <ideaplugins@gmail.com>
2018-04-25 17:14:34 -0300

    Refactor

--- src/test/java/ar/edu/undav/semillero/service/StudentServiceTest.java
+++ src/test/java/ar/edu/undav/semillero/service/StudentServiceTest.java
@@ -18,6 +18,7 @@ import org.springframework.boot.test.rule.OutputCapture;
 import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.core.io.Resource;
 
+import java.util.Arrays;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.StreamSupport;
@@ -52,15 +53,16 @@ public class StudentServiceTest {
     public void testImportCSV() {
         Mockito.when(nodeRepository.findByName(Mockito.eq("Capital"))).thenReturn(Optional.of(new Node("Capital", "address")));
         Mockito.when(nodeRepository.findByName(Mockito.eq("Paternal"))).thenReturn(Optional.empty());
-        Resource resource = new DefaultResourceLoader().getResource("classpath:egresados.csv");
+        Resource resource = new DefaultResourceLoader().getResource("classpath:data.csv");
         studentService.importCSV(resource);
-        Mockito.verify(nodeRepository, Mockito.times(51)).findByName(Mockito.eq("Capital"));
-        Mockito.verify(nodeRepository).findByName(Mockito.eq("Paternal"));
+        Mockito.verify(nodeRepository, Mockito.times(46)).findByName(Mockito.eq("Capital"));
+        Mockito.verify(nodeRepository, Mockito.times(4)).findByName(Mockito.eq("Paternal"));
         ArgumentCaptor<Iterable<Student>> captor = ArgumentCaptor.forClass(Iterable.class);
         Mockito.verify(studentRepository).saveAll(captor.capture());
-        Assertions.assertThat(captor.getValue()).hasSize(51);
+        Assertions.assertThat(captor.getValue()).hasSize(46);
         long graduatedCount = StreamSupport.stream(captor.getValue().spliterator(), true).filter(s -> Objects.nonNull(s.getGraduationDate())).count();
-        Assertions.assertThat(graduatedCount).isEqualTo(18);
-        Assertions.assertThat(capture.toString()).contains("Node not found Paternal");
+        Assertions.assertThat(graduatedCount).isEqualTo(20);
+        long count = Arrays.stream(capture.toString().split("\n")).filter(s -> s.endsWith("Node not found Paternal")).count();
+        Assertions.assertThat(count).isEqualTo(4L);
     }
 }
